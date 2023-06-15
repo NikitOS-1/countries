83c5f0aea02d6c239e59ebb43773fd05bd812f95
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-15 21:14:57 +0300

    add fn toggleTheme

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import style from "./App.module.scss";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  console.log(theme);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <div className={"dark"}>
-      <Header />
+    <div
+      className={[
+        style["container"],
+        `${theme ? style["light"] : style["dark"]}`,
+      ].join(" ")}>
+      <Header toggleTheme={toggleTheme} />
       <Main />
       <Footer />
     </div>
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -2,11 +2,11 @@ import { IoMoonOutline, IoMoon } from "react-icons/io5";
 import style from "./Header.module.scss";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ toggleTheme }) => {
   return (
     <header>
       <a href="">Where is the world?</a>
-      <div>
+      <div onClick={toggleTheme}>
         <IoMoon />
         <span>Light Mode </span>
       </div>
