50d997149713c712c68407d1b91a0deeb2353cde
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-15 17:39:10 +0300

    ref style and add new file with var

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,6 @@
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-import Main from "./Main/Main";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
   return (
