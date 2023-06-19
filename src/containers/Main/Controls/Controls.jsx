fd71892aa977b1cb95e336da5ef54d4f2bfcde6d
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-19 21:14:17 +0300

    add material and new Select

--- src/containers/Main/Controls/Controls.jsx
+++ src/containers/Main/Controls/Controls.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import Search from "../../../components/Search/Search";
+import CustomSelect from "../../../components/CustomSelect/CustomSelect";
+import style from "./Controls.module.scss";
 
 const Controls = () => {
   const [search, setSearch] = useState("");
   return (
-    <div>
+    <div className={style.container}>
       <Search search={search} setSearch={setSearch} />
+      <CustomSelect />
     </div>
   );
 };
