fc23e4405f729bac7a2362048d78fe61c2c10431
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-20 18:33:19 +0300

    add Cards and responsed style

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,6 @@
+import style from "./Card.module.scss";
+
+const Card = () => {
+  return <div className={style.wrap}>Card</div>;
+};
+export default Card;
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,3 +1,4 @@
+import Card from "../../components/Card/Card";
 import Search from "../../components/Search/Search";
 import Controls from "./Controls/Controls";
 import style from "./Main.module.scss";
@@ -6,6 +7,15 @@ const Main = () => {
   return (
     <main className={style.wrap}>
       <Controls />
+      <div className={style.cards}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </main>
   );
 };
