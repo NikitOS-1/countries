8f27073bfc4b38b08c8a897d4a160fe38a409b75
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-28 17:42:03 +0300

    add signature

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,4 +1,10 @@
+import style from "./Footer.module.scss";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className={style.after}>© 2023 Country. Nikita Puzyrenko</div>
+    </footer>
+  );
 };
 export default Footer;
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -2,30 +2,30 @@ import axios from "axios";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
 import { useEffect, useState } from "react";
+import { searchByCountry } from "../../config";
 
-const Details = ({ match }) => {
+const Details = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { name } = useParams();
   const path = location.pathname;
-
   const [country, setCountry] = useState(null);
+  console.log(country);
 
   useEffect(() => {
-    axios.get();
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
   }, [name]);
 
-  const goBack = () => {
-    navigate(-1);
-  };
   return (
     <>
       <div>
-        <button onClick={goBack}>
-          <IoArrowBack />
-          Back
-        </button>
-        {name}
+        <nav>
+          <button onClick={() => navigate(-1)}>
+            <IoArrowBack />
+            Back
+          </button>
+        </nav>
+        <div>{name}</div>
       </div>
     </>
   );
