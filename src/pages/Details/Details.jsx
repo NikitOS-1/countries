0c81315cf51a71bfd2a71051e82eafdc8bcacf67
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-06-28 17:00:01 +0300

    ref config

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,9 +1,33 @@
-import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { useEffect, useState } from "react";
 
-const Details = () => {
+const Details = ({ match }) => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const { name } = useParams();
   const path = location.pathname;
 
-  return <div>Details{path}</div>;
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    axios.get();
+  }, [name]);
+
+  const goBack = () => {
+    navigate(-1);
+  };
+  return (
+    <>
+      <div>
+        <button onClick={goBack}>
+          <IoArrowBack />
+          Back
+        </button>
+        {name}
+      </div>
+    </>
+  );
 };
 export default Details;
